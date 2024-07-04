184b1d39998679bd39889e97dbbf1e87d1640546
narjoDev <63086420+narjoDev@users.noreply.github.com>
2024-07-03 22:32:18 -0700

    Follow videos through validation

--- todo.rb
+++ todo.rb
@@ -1,7 +1,50 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View list of lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists
+end
+
+# Render the new list form
+get '/lists/new' do
+  erb :new_list
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique'
+  end
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params['list_name'].strip
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list # address still displays '/lists'...
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
 end
